46503db35bf90d0e7de3c733fe543bbe598cf08b
Remco Vermeulen <rvermeulen@gmail.com>
2014-08-03 01:26:29 +0200

    Added reflection utility class

---
+++ MessageBird/Utilities/ReflectionUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace MessageBird.Utilities
+{
+    public class ReflectionUtils
+    {
+        public static bool IsNullable(Type t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("t");
+            }
+
+            return (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+    }
+}
