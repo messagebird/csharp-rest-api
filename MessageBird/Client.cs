1e6f00c463c1a511ef99ff9409a7a16c5149acd3
Remco Vermeulen <rvermeulen@gmail.com>
2014-08-03 01:33:36 +0200

    Added preliminary client implementation

--- MessageBird/Client.cs
+++ MessageBird/Client.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MessageBird.Objects;
+using MessageBird.Resources;
+using MessageBird.Net;
 
 namespace MessageBird
 {
     public class Client
     {
+        private IRestClient restClient;
+        private Client(IRestClient restClient)
+        {
+            this.restClient = restClient;
+        }
+
+        public static Client Create(IRestClient restClient)
+        {
+            return new Client(restClient);
+        }
+
+        public static Client CreateDefault(string accessKey)
+        {
+            return new Client(new RestClient(accessKey));
+        }
+
+        public Message SendMessage(Message message)
+        {
+            Messages messageToSend = new Messages(message);
+            Messages result = (Messages)restClient.Create(messageToSend);
+
+            return result.Message;
+        }
+
+        public Message ViewMessage(string id)
+        {
+            Messages messageToView = new Messages(id);
+            Messages result = (Messages)restClient.Retrieve(messageToView);
+
+            return result.Message;
+        }
     }
 }
