7765b6e8472b6294a403aef3d2ef626bbbd4dd66
Remco Vermeulen <rvermeulen@gmail.com>
2014-08-03 01:31:49 +0200

    Added error object

---
+++ MessageBird/Objects/Error.cs
@@ -0,0 +1,16 @@
+﻿namespace MessageBird.Objects
+{
+    public class Error
+    {
+        private int code;
+        private string description;
+        private string parameter;
+
+        public Error(int code, string description, string parameter)
+        {
+            this.code = code;
+            this.description = description;
+            this.parameter = parameter;
+        }
+    }
+}
