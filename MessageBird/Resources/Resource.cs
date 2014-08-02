2f41bc3967f41ea5443320eb18a634c99765b25f
Remco Vermeulen <rvermeulen@gmail.com>
2014-08-03 01:24:52 +0200

    Added resource interface

---
+++ MessageBird/Resources/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace MessageBird.Resources
+{
+    public interface IResource
+    {
+        string Id { get; }
+        string Name { get; }
+
+        void FromResource(string resource);
+        string ToResource();
+    }
+}
