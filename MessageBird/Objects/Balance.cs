6dc8c829f48cb8f786c83093f29f799424e4bda8
Remco Vermeulen <rvermeulen@gmail.com>
2014-08-07 22:14:12 +0200

    Explain why balance has an id that is always null

--- MessageBird/Objects/Balance.cs
+++ MessageBird/Objects/Balance.cs
@@ -23,6 +23,13 @@ namespace MessageBird.Objects
 
     public class Balance : IIdentifiable<string>
     {
+        /// <summary>
+        /// To uniformly treat objects, we implement the IIdentifiable interface even though
+        /// a Balance object doesn't have an id!
+        /// By returning null, the Balance object signals the user of a balance object
+        /// (currently always the balance resource) to ignore the id property.
+        /// </summary>
+        /// <remarks>Throwing an exception will interfer with serialization of a balance object.</remarks>
         public string Id
         {
             get
