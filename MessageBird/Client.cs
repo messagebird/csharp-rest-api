aab225015f874d1e9498a9da799bda9a3c6361d2
Remco Vermeulen <rvermeulen@gmail.com>
2014-08-08 09:30:42 +0200

    Perform basic parameter validation

--- MessageBird/Client.cs
+++ MessageBird/Client.cs
@@ -2,6 +2,7 @@
 using MessageBird.Objects;
 using MessageBird.Resources;
 using MessageBird.Net;
+using MessageBird.Utilities;
 
 namespace MessageBird
 {
@@ -16,16 +17,24 @@ namespace MessageBird
 
         public static Client Create(IRestClient restClient)
         {
+            ParameterValidator.IsNotNull(restClient, "restClient");
+
             return new Client(restClient);
         }
 
         public static Client CreateDefault(string accessKey, ICredentials proxyCredentials)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(accessKey, "accessKey");
+
             return new Client(new RestClient(accessKey, proxyCredentials));
         }
 
         public Message SendMessage(string originator, string body, long[] msisdns, MessageOptionalArguments optionalArguments = null)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(originator, "originator");
+            ParameterValidator.IsNotNullOrWhiteSpace(body, "body");
+            ParameterValidator.ContainsAtLeast(msisdns, 1, "msisdns");
+
             var recipients = new Recipients(msisdns);
             var message = new Message(originator, body, recipients, optionalArguments);
 
@@ -37,6 +46,8 @@ namespace MessageBird
 
         public Message ViewMessage(string id)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(id, "id");
+
             var messageToView = new Messages(new Message(id));
             var result = restClient.Retrieve(messageToView);
 
@@ -45,6 +56,9 @@ namespace MessageBird
 
         public VoiceMessage SendVoiceMessage(string body, long[] msisdns, VoiceMessageOptionalArguments optionalArguments = null)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(body, "body");
+            ParameterValidator.ContainsAtLeast(msisdns, 1, "msisdns");
+
             var recipients = new Recipients(msisdns);
             var voiceMessage = new VoiceMessage(body, recipients, optionalArguments);
             var voiceMessages = new VoiceMessages(voiceMessage);
@@ -55,6 +69,8 @@ namespace MessageBird
 
         public VoiceMessage ViewVoiceMessage(string id)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(id, "id");
+
             var voiceMessageToView = new VoiceMessages(new VoiceMessage(id));
             var result = restClient.Retrieve(voiceMessageToView);
 
@@ -63,6 +79,8 @@ namespace MessageBird
 
         public Objects.Hlr RequestHlr(long msisdn, string reference)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(reference, "reference");
+
             var hlrToRequest = new Resources.Hlr(new Objects.Hlr(msisdn, reference));
             var result = restClient.Create(hlrToRequest);
 
@@ -71,6 +89,8 @@ namespace MessageBird
 
         public Objects.Hlr ViewHlr(string id)
         {
+            ParameterValidator.IsNotNullOrWhiteSpace(id, "id");
+
             var hlrToView = new Resources.Hlr(new Objects.Hlr(id));
             var result = restClient.Retrieve(hlrToView);
 
