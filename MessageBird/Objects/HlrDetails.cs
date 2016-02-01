627a0a2c2622563d85098475d840c8473015e2b6
Sam Wierema <sam@messagebird.com>
2016-02-01 06:16:32 -0800

    Add the details attribute to the HLR object

---
+++ MessageBird/Objects/HlrDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace MessageBird.Objects
+{
+    public class HlrDetails
+    {
+        [JsonProperty("status_desc")]
+        public string StatusDesc { get; set; }
+
+        [JsonProperty("imsi")]
+        public string Imsi { get; set; }
+
+        [JsonProperty("country_iso")]
+        public string CountryIso { get; set; }
+
+        [JsonProperty("country_name")]
+        public string CountryName { get; set; }
+
+        [JsonProperty("location_msc")]
+        public string LocationMsc { get; set; }
+
+        [JsonProperty("location_iso")]
+        public string LocationIso { get; set; }
+
+        [JsonProperty("ported")]
+        public int Ported { get; set; }
+
+        [JsonProperty("roaming")]
+        public int Roaming { get; set; }
+    }
+}
